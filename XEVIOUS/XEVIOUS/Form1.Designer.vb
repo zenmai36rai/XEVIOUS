99f78ad2ff5baca8d9b0b0e2092a2f6417cfff9b
Kyosuke Miyazawa <31465059+zenmai36rai@users.noreply.github.com>
2025-01-29 13:53:39 +0900

    XEVIOUS NEW PROJECT

---
+++ XEVIOUS/XEVIOUS/Form1.Designer.vb
@@ -0,0 +1,57 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(9, 13)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(347, 425)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(369, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ XEVIOUS/XEVIOUS/Form1.vb
@@ -0,0 +1,47 @@
+﻿' XEVIOUS LIKE STG EDIT BY KYOSUKE MIYAZAWA COPYRIGHT SINCE 2025
+Public Class Form1
+    Private bmp As Bitmap
+    Private fgt As Bitmap
+    Private sx = 0
+    Private sx2 = 0
+    Private sy = 0
+    Private _g As Graphics
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim canvas As Bitmap = New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        _g = Graphics.FromImage(canvas)
+        sy = sy + 4
+        If sy = bmp.Height Then
+            sy = 0
+            sx = sx + canvas.Width
+            If sx + canvas.Width > bmp.Width Then
+                sx = 0
+            End If
+        End If
+        Dim y = bmp.Height - PictureBox1.Height - sy
+        Dim src1 = New Rectangle(sx, y, PictureBox1.Width, PictureBox1.Height)
+        Dim dst = New Rectangle(0, 0, PictureBox1.Width, PictureBox1.Height)
+        _g.DrawImage(bmp, dst, src1, GraphicsUnit.Pixel)
+        If y < PictureBox1.Height Then
+            sx2 = sx + canvas.Width
+            If sx2 + canvas.Width > bmp.Width Then
+                sx2 = 0
+            End If
+            Dim y2 = y + bmp.Height
+            Dim src2 = New Rectangle(sx2, y2, PictureBox1.Width, PictureBox1.Height)
+            _g.DrawImage(bmp, dst, src2, GraphicsUnit.Pixel)
+        End If
+        _g.DrawImage(bmp, dst, src1, GraphicsUnit.Pixel)
+        Dim fx As Integer = (PictureBox1.Width / 2) - 24
+        Dim fy As Integer = PictureBox1.Height - 100
+        _g.DrawImage(fgt, fx, fy)
+        _g.Dispose()
+        PictureBox1.Image = canvas
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        bmp = New Bitmap("STG_MAP.png")
+        fgt = New Bitmap("STG_SOL.png")
+        Timer1.Start()
+    End Sub
+End Class
